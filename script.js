032086fe5484996fb5933c5cf7bb3f353114c19f
lukaszparadylo <lukaszparadylo@gmail.com>
2022-07-06 17:00:48 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//let username = window.prompt("what's your name?");
+//console.log(username);
+let x = 3.14;
+let y = 5;
+let z = 6;
+x = Math.round(x);
+x = Math.max(x,y,z);
+console.log(x);
---
+++ test.js
@@ -0,0 +1,15 @@
+function eat(){
+    console.log("Mniam mniam");
+}
+function add(a,b){
+    return a+b;
+}
+
+var Pikachu = {
+    name: "Pikachu",
+    image: "obraz.jpg",
+    attack: 9,
+    speak: function(message){
+        console.log(message)
+    }
+};
